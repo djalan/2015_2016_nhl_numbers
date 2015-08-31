277a71c54326d54b72239a071730ef44f3d0fcec
Alain Sirois <alainsirois@gmail.com>
2015-08-30 23:24:41 -0400

    bad salary extract, fixed! BUT must update DB with new salary

--- ex_nn.rb
+++ ex_nn.rb
@@ -7,33 +7,34 @@ require 'nokogiri'
 
 
 teams = %W{ANA ARI BOS BUF CGY CAR CHI COL CLB DAL DET EDM FLA LAK MIN MTL NAS NJD NYI NYR OTT PHI PIT SJS STL TBL TOR VAN WAS WPG}
-#teams = %W{ANA}
+#teams = %W{TBL}
 
 teams.each do |team|
-  #puts "--------#{team}---------"
+  puts "--------#{team}---------"
 	html = Nokogiri::HTML(File.open(File.expand_path("get/nn_#{team}.html")))
-
-  players = []
-  html.css('a[class = "active"]').each do |player|
-    name = /(?<last>[A-Za-z\-]*),.(?<first>[A-Za-z\-]*)/.match(player.text)
-    unless name
-      name = /(?<first>[A-Za-z\-]*) (?<last>[A-Za-z\-]*)/.match(player.text)
+  
+  go = false
+  html.xpath('//table//tbody//tr').each do |row| 
+    header = row.css('td[colspan="3"]')
+    case header.text
+    when "Forwards", "Defencemen", "Goaltenders"
+      go = true
+      next
+    when "Other"
+      break
     end
-    name2 =  "#{name['first']} #{name['last']}"
-    players.push(name2)
+    
+    if go
+      name = /(?<last>[A-Za-z\-]*),.(?<first>[A-Za-z\-]*)/.match( row.css('td a').text )
+      unless name
+        name = /(?<first>[A-Za-z\-]*) (?<last>[A-Za-z\-]*)/.match( row.css('td a').text )
+      end
+      puts "#{name['first']} #{name['last']};#{(row.css('td[class="caphit"]').text.to_f * 1_000_000).to_i};#{team}"
+    end
+    
   end
   
-  salaries = []
-  html.css('td[class = "caphit"]').each do |salary|
-    salaries.push((salary.text.to_f * 1_000_000).to_i)
-  end
-  salaries2 = salaries[0...players.size]
-  
-  players.zip(salaries2).each do |name, salary|
-    puts "#{name};#{salary};#{team}"
-  end
-  
-end
+end #teams.each
 
 
 
